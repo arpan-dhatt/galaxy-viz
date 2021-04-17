ee6e3dcb74f01281693cf6d4bd81ee6478d631ff
Arpan Dhatt <ary.dhatt@gmail.com>
2021-04-17 17:01:32 -0500

    added temporary local simulation code for testing

--- GalaxyViz/Shaders.metal
+++ GalaxyViz/Shaders.metal
@@ -8,7 +8,99 @@
 #include <metal_stdlib>
 using namespace metal;
 
-kernel void black_placeholder_func(texture2d<half, access::read_write> tex [[ texture(0) ]],
-                                   uint id [[ thread_position_in_grid ]]) {
-    
+struct Particle{
+    float4 color;
+    float3 position;
+    float3 velocity;
+    float mass;
+};
+
+struct SimulationConstants {
+    float gravitational_constant;
+    float softening_factor;
+    float dt;
+    float angleX;
+    float angleZ;
+    float scale;
+};
+
+kernel void clear_pass_func(texture2d<half, access::write> tex [[ texture(0) ]],
+                            uint2 id [[ thread_position_in_grid ]]){
+    tex.write(half4(0), id);
 }
+
+struct AccOut {
+    float3 acc;
+    float accMag;
+};
+
+AccOut acceleration(device Particle *particles, uint id, int particleCount, SimulationConstants simConsts) {
+    Particle particle1 = particles[id];
+    float3 total_acc = float3(0);
+//    float totalMag = 0;
+    for (uint i = 0; i < uint(particleCount); i++) {
+        Particle particle2 = particles[i];
+        float3 acc = particle2.position - particle1.position;
+        acc /= 2;
+        float magSquared = acc.x*acc.x + acc.y*acc.y + acc.z*acc.z;
+        float inv_r3 = pow(magSquared + simConsts.softening_factor*simConsts.softening_factor, 1.5);
+        acc /= inv_r3;
+        acc *= particle2.mass+0.001;
+//        totalMag += sqrt(magSquared);
+        total_acc += acc;
+    }
+    return {total_acc*simConsts.gravitational_constant, 0};
+}
+
+float3 rotateZ(float3 point, float angle) {
+    float x = point.x;
+    float y = point.y;
+    float z = point.z;
+    return {x*cos(angle) - y*sin(angle), x*sin(angle) + y*cos(angle), z};
+}
+
+float3 rotateX(float3 point, float angle) {
+    float x = point.x;
+    float y = point.y;
+    float z = point.z;
+    return {x, y*cos(angle) - z*sin(angle), y*sin(angle) + z*cos(angle)};
+}
+
+kernel void draw_dots_func(device Particle *particles [[ buffer(0) ]],
+                           constant int &particleCount [[ buffer(1) ]],
+                           constant SimulationConstants &simulationConstants [[ buffer(2) ]],
+                           texture2d<half, access::read_write> tex [[ texture(0) ]],
+                           uint id [[ thread_position_in_grid ]]){
+    
+    Particle particle;
+    particle = particles[id];
+    
+    float3 position = particle.position;
+    float3 velocity = particle.velocity;
+    AccOut accOut = acceleration(particles, id, particleCount, simulationConstants);
+    float3 acc = accOut.acc;
+//    float totalMag = accOut.accMag;
+    velocity += acc*simulationConstants.dt;
+    position += velocity*simulationConstants.dt;
+    
+//    if(position.x < 0 || position.x > width) velocity.x *= -1;
+//    if(position.y < 0 || position.y > height) velocity.y *= -1;
+    
+    particle.position = position;
+    particle.velocity = velocity;
+    
+    particles[id] = particle;
+    float3 outPos = rotateZ(position, simulationConstants.angleZ);
+    outPos = rotateX(outPos, simulationConstants.angleX);
+    outPos *= simulationConstants.scale;
+    uint2 texturePosition = uint2(outPos.x+tex.get_width()/2, outPos.y+tex.get_height()/2);
+//    half velMag = velocity.x*velocity.x + velocity.y*velocity.y + velocity.z*velocity.z;
+    half4 col = half4(1, 1, 1, 1);
+    tex.write(tex.read(texturePosition)+col, texturePosition);
+//    tex.write(tex.read(texturePosition + uint2(1,0))+col, texturePosition + uint2(1,0));
+//    tex.write(tex.read(texturePosition + uint2(0,1))+col, texturePosition + uint2(0,1));
+//    tex.write(tex.read(texturePosition - uint2(1,0))+col, texturePosition - uint2(1,0));
+//    tex.write(tex.read(texturePosition - uint2(0,1))+col, texturePosition - uint2(0,1));
+}
+
+
--- GalaxyViz/SimulationMetalView.swift
+++ GalaxyViz/SimulationMetalView.swift
@@ -9,6 +9,22 @@ import SwiftUI
 import UIKit
 import MetalKit
 
+struct Particle{
+    var color: SIMD4<Float>
+    var position: SIMD3<Float>
+    var velocity: SIMD3<Float>
+    var mass: Float
+}
+
+struct SimulationConstants {
+    var gravitational_constant: Float
+    var softening_factor: Float
+    var dt: Float
+    var angleX: Float
+    var angleZ: Float
+    var scale: Float
+}
+
 struct SimulationMetalView {
     
     func makeCoordinator() -> Coordinator {
@@ -31,25 +47,83 @@ struct SimulationMetalView {
     }
     
     class Coordinator: NSObject, MTKViewDelegate {
+        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+        
+        func draw(in view: MTKView) {
+            guard let drawable = view.currentDrawable else { return }
+            
+            let commandbuffer = commandQueue.makeCommandBuffer()
+            let computeCommandEncoder = commandbuffer?.makeComputeCommandEncoder()
+            
+            computeCommandEncoder?.setComputePipelineState(clearPass)
+            computeCommandEncoder?.setTexture(drawable.texture, index: 0)
+
+            let w = clearPass.threadExecutionWidth
+            let h = clearPass.maxTotalThreadsPerThreadgroup / w
+            
+            var threadsPerThreadGroup = MTLSize(width: w, height: h, depth: 1)
+            var threadsPerGrid = MTLSize(width: drawable.texture.width, height: drawable.texture.height, depth: 1)
+            computeCommandEncoder?.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerThreadGroup)
+
+            computeCommandEncoder?.setComputePipelineState(drawDotPass)
+            computeCommandEncoder?.setBuffer(particleBuffer, offset: 0, index: 0)
+            computeCommandEncoder?.setBytes(&particleCount, length: MemoryLayout<Int>.size, index: 1)
+            computeCommandEncoder?.setBytes(&simulationConstants, length: MemoryLayout<SimulationConstants>.stride, index: 2)
+            threadsPerGrid = MTLSize(width: Int(particleCount), height: 1, depth: 1)
+            threadsPerThreadGroup = MTLSize(width: w, height: 1, depth: 1)
+            computeCommandEncoder?.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerThreadGroup)
+
+            computeCommandEncoder?.endEncoding()
+            commandbuffer?.present(drawable)
+            commandbuffer?.commit()
+            
+            if ZDown {
+                simulationConstants.angleZ += 0.05
+            }
+            if XDown {
+                simulationConstants.angleX += 0.05
+            }
+            if CDown {
+                simulationConstants.scale *= 1.05
+            }
+            if VDown {
+                simulationConstants.scale /= 1.05
+            }
+            simulationConstants.angleX += 0.005
+        }
+        
         var parent: SimulationMetalView
         var metalDevice: MTLDevice!
         
-        var metalCommandQueue: MTLCommandQueue!
+        var commandQueue: MTLCommandQueue!
         var clearPass: MTLComputePipelineState!
         var drawDotPass: MTLComputePipelineState!
         
         var particleBuffer: MTLBuffer!
+        var simulationConstants = SimulationConstants(gravitational_constant: 6.674, softening_factor: 10, dt: 1/2400, angleX: 0, angleZ: 0, scale: 1)
+        
+        var particleCount: Int = 10000
+        var initial_velocity: Float = 2000000
+        var screenSize: Float = 1000
+        
+        var ZDown: Bool = false
+        var XDown: Bool = false
+        var CDown: Bool = false
+        var VDown: Bool = false
+        
         
         init(_ parent: SimulationMetalView) {
             self.parent = parent
             if let metalDevice = MTLCreateSystemDefaultDevice() {
                 self.metalDevice = metalDevice
             }
-            self.metalCommandQueue = metalDevice.makeCommandQueue()!
+            
+            self.commandQueue = metalDevice.makeCommandQueue()
             
             let library = metalDevice.makeDefaultLibrary()
             let clearFunc = library?.makeFunction(name: "clear_pass_func")
-            let drawDotFunc = library?.makeFunction(name: "draw_dot_func")
+            let drawDotFunc = library?.makeFunction(name: "draw_dots_func")
+            
             do{
                 clearPass = try metalDevice.makeComputePipelineState(function: clearFunc!)
                 drawDotPass = try metalDevice.makeComputePipelineState(function: drawDotFunc!)
@@ -57,24 +131,66 @@ struct SimulationMetalView {
                 print(error)
             }
             super.init()
+            createParticles()
         }
         
-        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
-        
-        func draw(in view: MTKView) {
-            guard let drawable = view.currentDrawable else {
-                return
+        func createParticles(){
+            var particles: [Particle] = []
+            for i in 0..<particleCount{
+                let red: Float = Float(arc4random_uniform(100)) / 100
+                let green: Float = Float(arc4random_uniform(100)) / 100
+                let blue: Float = Float(arc4random_uniform(100)) / 100
+                let distance = Float(arc4random_uniform(UInt32(screenSize)/2))
+                
+                var position = SIMD3<Float>(Float.random(in: -1...1),Float.random(in: -1...1),Float.random(in: -1...1))
+    //            position /= pow(position.x*position.x + position.y*position.y + position.z*position.z, 0.5)
+                var velocity = SIMD3<Float>(Float.random(in: -1...1),Float.random(in: -1...1),0)
+                velocity /= pow(velocity.x*velocity.x + velocity.y*velocity.y + velocity.z*velocity.z, 0.5)
+                velocity *= 20
+                
+                let angle = Float.random(in: 0...(2*Float.pi))
+                position.x = cos(angle) * distance
+                position.y = sin(angle) * distance
+                position.z *= 100
+
+                velocity.x = cos(angle+Float.pi/2)
+                velocity.y = sin(angle+Float.pi/2)
+                velocity *= initial_velocity/(5000+distance/2000)
+                velocity.z = Float.random(in: -0.1...0.1)
+                
+                
+                let particle = Particle(color: SIMD4<Float>(red, green, blue, 1),
+                                        position: position,
+                                        velocity: velocity,
+                                        mass: 1.98e3)
+    //            if i == 0 {
+    //                particle.position = SIMD3<Float>(screenSize, 0, 0)
+    //                particle.velocity *= 0
+    //                particle.mass *= 10000
+    //            }
+                particles.append(particle)
             }
+            particleBuffer = metalDevice.makeBuffer(bytes: particles, length: MemoryLayout<Particle>.stride * Int(particleCount), options: .storageModeManaged)
+        }
+        
+        func createParticlesFibonacci() {
+            var particles: [Particle] = []
+            let goldenRatio: Float = (1.0+sqrt(5.0))/2.0
+            let angleIncrement = Float.pi * goldenRatio
             
-            let commandBuffer = metalCommandQueue.makeCommandBuffer()
-            let rpd = view.currentRenderPassDescriptor
-            rpd?.colorAttachments[0].clearColor = MTLClearColorMake(0.43, 0.73, 0.35, 1.0)
-            rpd?.colorAttachments[0].loadAction = .clear
-            rpd?.colorAttachments[0].storeAction = .store
-            let re = commandBuffer?.makeRenderCommandEncoder(descriptor: rpd!)
-            re?.endEncoding()
-            commandBuffer?.present(drawable)
-            commandBuffer?.commit()
+            for i in 0..<particleCount {
+                let t = Float(i)/Float(particleCount)
+                let angle1 = acos(1-2*t)
+                let angle2 = angleIncrement * Float(i)
+                
+                let x = sin(angle1) * cos(angle2)
+                let y = sin(angle1) * sin(angle2)
+                let z = cos(angle1)
+                
+                let p = Particle(color: SIMD4<Float>.zero, position: SIMD3<Float>(x: x, y: y, z: z) * screenSize/2, velocity: SIMD3<Float>.zero, mass: 1.98e3)
+                particles.append(p)
+            }
+            particleBuffer = metalDevice.makeBuffer(bytes: particles, length: MemoryLayout<Particle>.stride * Int(particleCount), options: .storageModeManaged)
         }
     }
 }
