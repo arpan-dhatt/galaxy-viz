244f023a65c6cfa8a1e813aae2cd4a1e7d1c2d1a
Arpan Dhatt <ary.dhatt@gmail.com>
2021-04-17 11:43:52 -0500

    Initial Commit

---
+++ GalaxyViz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GalaxyViz
+//
+//  Created by Arpan Dhatt on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GalaxyViz/GalaxyVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyVizApp.swift
+//  GalaxyViz
+//
+//  Created by Arpan Dhatt on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
